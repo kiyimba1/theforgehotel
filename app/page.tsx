b19a7adcc64853b1325f8b5c2c96222cdf876bb7
Edwin Kiyimba <edwinkiyimba@gmail.com>
2023-12-06 21:04:09 +0300

    History component for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "./ui/HeroBarnner/Hero";
+import HistorySection from "./ui/HistorySection/HistorySection";
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Hero />
+			<HistorySection />
 		</div>
 	);
 }
---
+++ app/ui/HistorySection/HistorySection.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import styles from "./HistorySection.module.css";
+import Link from "next/link";
+
+const HistorySection = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.historyImage}>
+				<img src="/images/hotel_front.jpg" alt="Hotel Front" />
+			</div>
+			<div className={styles.historyContent}>
+				<h1>
+					<span className={styles.colored}>The History</span> Of Our Hotel.
+				</h1>
+
+				<div className={styles.details}>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+						voluptatem quam officiis reiciendis eius nulla aliquam similique
+						nisi illo impedit laborum, laboriosam voluptatum quasi veritatis
+						quidem voluptates velit totam doloremque.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+						doloremque nulla accusamus exercitationem. Accusantium tenetur,
+						optio dignissimos rerum obcaecati commodi.
+					</p>
+				</div>
+
+				<Link href="/about" className={styles.button}>
+					Read More
+				</Link>
+			</div>
+		</div>
+	);
+};
+export default HistorySection;
